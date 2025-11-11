4429f57c02cc2332e4a0c6685485106db1df9151
Federico0284364 <tabella.federico@gmail.com>
2025-11-11 17:35:15 +0100

    added types to score-history table

--- src/components/ScoreHistoryTable.tsx
+++ src/components/ScoreHistoryTable.tsx
@@ -1,17 +1,20 @@
-export default function ScoreHistoryTable({
-	scoreHistory,
-	bestEntry,
-}) {
-	
+import { ScoreEntry } from "../pages/ScoreHistory";
+import { twMerge } from "tailwind-merge";
 
+type Props = {
+	scoreHistory: ScoreEntry[];
+	bestEntry: ScoreEntry;
+};
+
+export default function ScoreHistoryTable({ scoreHistory, bestEntry }: Props) {
 	const headerCellStyle = " bg-black/20 rounded-lg";
 
 	return (
 		<table className=" min-w-[85vw] sm:min-w-[80vw] md:min-w-[35vw] w-[85%] rounded-2xl text-center border-separate border-spacing-1 table-fixed border-neutral-800 border-4 bg-orange-400 pb-1 px-1 shadow-md shadow-black/50">
 			<thead>
 				<tr>
-					<th className={"w-[20vw]" + headerCellStyle}>Date</th>
-					<th className={"w-[10vw]" + headerCellStyle}>Score</th>
+					<th className={twMerge("w-[20vw]", headerCellStyle)}>Date</th>
+					<th className={twMerge("w-[10vw]" + headerCellStyle)}>Score</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -29,16 +32,15 @@ export default function ScoreHistoryTable({
 							hour12: false, // opzionale, per ora 24h
 						}
 					);
-					cellStyle += bestEntry.score === entry.score
-						? " bg-green-500/60"
-						: " bg-neutral-700";
+					cellStyle +=
+						bestEntry.score === entry.score
+							? " bg-green-500/60"
+							: " bg-neutral-700";
 
 					return (
 						<tr className="bg-black/80">
 							<td className={cellStyle}>{formattedDate}</td>
-							<td className={cellStyle}>
-								{entry.score}
-							</td>
+							<td className={cellStyle}>{entry.score}</td>
 						</tr>
 					);
 				})}
