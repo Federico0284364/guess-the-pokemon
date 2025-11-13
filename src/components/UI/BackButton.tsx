627e77821ed5a5d98069ef6b1878a840f8ac2cbd
Federico0284364 <tabella.federico@gmail.com>
2025-11-13 10:47:28 +0100

    added types to BackButton

--- src/components/UI/BackButton.tsx
+++ src/components/UI/BackButton.tsx
@@ -1,6 +1,11 @@
+import { ButtonHTMLAttributes } from "react";
 import Button from "./Button";
 
-export default function BackButton({className, onClick}){
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+
+}
+
+export default function BackButton({className, onClick}: Props){
   return (
     <Button onClick={onClick} className={className + " active:opacity-70 hover:bg-neutral-300 hover:text-neutral-700 font-extrabold text-xl text-center py-1 w-9 aspect-square items-center rounded-sm bg-neutral-600 border-neutral-300"} variant="secondary">{'x'}</Button>
   )
